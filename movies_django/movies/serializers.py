f31d4ea9797ebd9bd420c45473e19c67c0814181
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-11-11 08:01:53 -0500

    removed movie_url field in Movie Serializer because it duplicates the wikipage field's content

--- movies_django/movies/serializers.py
+++ movies_django/movies/serializers.py
@@ -3,10 +3,6 @@ from .models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    movie_url = serializers.ModelSerializer.serializer_url_field(
-        view_name='movie_detail'
-    )
-
     class Meta:
         model = Movie
         fields = ('year',
